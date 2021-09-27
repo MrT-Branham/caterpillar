720acc1ecf97ef21076dd17ec833cb7b1e196172
Steven Turner <sturner@cuhsd.org>
2021-09-27 09:19:36 -0700

    Added second circle.

--- caterpillar.py
+++ caterpillar.py
@@ -6,7 +6,15 @@ painter = trtl.Turtle()
 # first circle
 painter.begin_fill()
 painter.color("blue")
+painter.circle(100)
+painter.end_fill()
 
+# second circle
+painter.penup()
+painter.forward(200)
+painter.pendown()
+painter.begin_fill()
+painter.color("red")
 painter.circle(100)
 painter.end_fill()
 
